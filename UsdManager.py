41cbbfc422343539990a0402460ceaf1c27d3646
Josh Polansky <josh@loupe.team>
2024-10-16 08:25:46 -0700

    Add change block

--- UsdManager.py
+++ UsdManager.py
@@ -125,10 +125,10 @@ class RuntimeUsd:
         with self._lock:
             flat = self._data_update
             self._data_update = dict()
-
-        for key, value in flat.items():
-            path = self.root_prim.GetPath()
-            full_key = path.pathString + "/" + "/".join(key.split("."))
+        with Sdf.ChangeBlock():
+            for key, value in flat.items():
+                path = self.root_prim.GetPath()
+                full_key = path.pathString + "/" + "/".join(key.split("."))
             with layer_context(self._stage, self.edit_layer):
                 set_prim_value(self._stage, full_key, key, value)
 
